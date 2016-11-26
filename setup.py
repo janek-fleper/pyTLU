2a8b378353f51b206844992c5bb6bce7527703bc
Janek Fleper <j.fleper@gmx.de>
2016-11-26 14:58:55 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyTLU',
+    version=0.1,
+    description='pyTLU - API for TLU FPGA',
+    url='https://github.com/janek-fleper/pyTLU',
+    author='Janek Fleper',
+    author_email='j.fleper@gmx.de',
+    packages=['tlu'],
+    zip_safe=False
+)
