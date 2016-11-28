d381fc95b0187d946c3e97e3a5aadba44989014e
Janek Fleper <j.fleper@gmx.de>
2016-11-28 19:41:33 +0100

    Rename tlu to ZestSC1

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='pyTLU',
+    name='pyZestSC1',
     version=0.1,
-    description='pyTLU - API for TLU FPGA',
-    url='https://github.com/janek-fleper/pyTLU',
+    description='pyZestSC1 - API for ZestSC1 FPGA',
+    url='https://github.com/janek-fleper/pyZestSC1',
     author='Janek Fleper',
     author_email='j.fleper@gmx.de',
     packages=['tlu'],
